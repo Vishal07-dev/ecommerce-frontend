e7d7a4327c4ac9a637b6e47e9db63255f6550aca
Vishal <vishalrohera07@gmail.com>
2025-06-10 18:22:56 +0530

    added new changes

--- src/components/wishlist-page/wishlist-page.component.ts
+++ src/components/wishlist-page/wishlist-page.component.ts
@@ -39,7 +39,7 @@ interface Product extends ApiProduct {
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [CommonModule, FormsModule,RouterLink],
+  imports: [CommonModule, FormsModule],
   templateUrl: './wishlist-page.component.html',
   styleUrl: './wishlist-page.component.css',
 })
